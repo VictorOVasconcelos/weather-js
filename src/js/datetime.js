f45cc5ee4dc6e99f1b12edd3d6dce0b30f7ac53c
todolist-jsnode <victorprogramadorvasconcelos@gmail.com>
2022-08-21 12:36:32 +0100

    Update datetime.js

--- src/js/datetime.js
+++ src/js/datetime.js
@@ -1,8 +1,9 @@
 const timeInfoHour = document.querySelector('.time-info-hour');
 const timeInfoDate = document.querySelector('.time-info-date');
 
+const newDate = new Date();
+
 const getCurrentHour = () => {
-    const newDate = new Date();
     const currentHours = newDate.getHours();
     const currentMinutes = newDate.getMinutes();
     const currentSeconds = newDate.getSeconds();
@@ -11,3 +12,38 @@ const getCurrentHour = () => {
     setTimeout('getCurrentHour()', 500);
 }
 getCurrentHour();
+
+const getCurrentDate = () => {
+    let dayCurrent;
+    const currentDay = newDate.getDay();
+
+    switch (currentDay) {
+        case 0:
+            dayCurrent = 'Sunday';
+            break;
+        case 1:
+            dayCurrent = 'Monday';
+            break;
+        case 2:
+            dayCurrent = 'Tuesday';
+            break;
+        case 3:
+            dayCurrent = 'Wednesday';
+            break;
+        case 4:
+            dayCurrent = 'Thursday';
+            break;
+        case 5:
+            dayCurrent = 'Friday';
+            break;
+        case 6:
+            dayCurrent = 'Saturday';
+            break;
+        default:
+            dayCurrent = 'Error';
+            break;
+    }
+
+    console.log(dayCurrent);
+}
+getCurrentDate();
