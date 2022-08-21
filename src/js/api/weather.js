4407b2e24d2a6b43a7db03fbbf07f40f3a154bf4
todolist-jsnode <victorprogramadorvasconcelos@gmail.com>
2022-08-21 11:54:59 +0100

    Update weather.js

--- src/js/api/weather.js
+++ src/js/api/weather.js
@@ -17,8 +17,23 @@ const createAPILogic = () => {
         const cityLatitude = geocodingData[0].lat;
         const cityLongitude = geocodingData[0].lon;
 
+        console.log(geocodingData[0]);
+
         cityInfoName.innerHTML = `${geocodingData[0].name}`;
-        countryInfoName.innerHTML = `${geocodingData[0].country}`;
+
+        const getCountryData = async () => {
+            const countryName = geocodingData[0].country;
+            console.log(countryName);
+            const countryURL = `https://restcountries.com/v3.1/alpha/${countryName}`;
+
+            const countryFetch = await fetch(countryURL);
+            const countryData = await countryFetch.json();
+
+            console.log(countryData);
+
+            countryInfoName.innerHTML = `${countryData[0].name.common}`;
+        }
+        getCountryData();
 
         const getWeatherData = async () => {
             const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLatitude}&lon=${cityLongitude}&appid=${keyAPI}`;
