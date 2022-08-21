915688ffad09ac7ddcf510c9f60d2d896b83ed6e
todolist-jsnode <victorprogramadorvasconcelos@gmail.com>
2022-08-21 11:40:05 +0100

    Update weather.js

--- src/js/api/weather.js
+++ src/js/api/weather.js
@@ -3,14 +3,14 @@ const keyAPI = `5f368db2d8ee713b9759b96de62cbf6e`;
 const searchInput = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-button');
 const cityInfoName = document.querySelector('.city-info-name');
-const countryInfoName = document.querySelector('.search-input-country');
+const countryInfoName = document.querySelector('.country-info-name');
 
 const createAPILogic = () => {
     const cityName = searchInput.value;
 
     const getPositionData = async () => {
         const geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${keyAPI}`;
-        
+
         const geocodingFetch = await fetch(geocodingURL);
         const geocodingData = await geocodingFetch.json();
 
@@ -18,6 +18,7 @@ const createAPILogic = () => {
         const cityLongitude = geocodingData[0].lon;
 
         cityInfoName.innerHTML = `${geocodingData[0].name}`;
+        countryInfoName.innerHTML = `${geocodingData[0].country}`;
 
         const getWeatherData = async () => {
             const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLatitude}&lon=${cityLongitude}&appid=${keyAPI}`;
