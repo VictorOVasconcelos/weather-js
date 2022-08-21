d615bae09e545eb9a5c251c16bb079965d7253d5
todolist-jsnode <victorprogramadorvasconcelos@gmail.com>
2022-08-21 17:35:10 +0100

    Update datetime.js

--- src/js/datetime.js
+++ src/js/datetime.js
@@ -1,28 +1,16 @@
-const timeInfoHour = document.querySelector('.time-info-hour');
-const timeInfoDate = document.querySelector('.time-info-date');
-
-const getCurrentHour = () => {
-    const newDate = new Date();
-    const currentHours = newDate.getHours();
-    const currentMinutes = newDate.getMinutes();
-    const currentSeconds = newDate.getSeconds();
-
-    timeInfoHour.innerHTML = `${currentHours}.${currentMinutes}:${currentSeconds}`;
-    setTimeout('getCurrentHour()', 500);
-}
-getCurrentHour()
+const mainDateValue = document.querySelector('.main-date-value');
 
 const getCurrentDate = () => {
     const newDate = new Date();
     const currentDay = newDate.getDay();
     const currentMonth = newDate.getMonth();
-    const currentYear = newDate.getFullYear();
+    const currentDayMonth = newDate.getDate() + 1;
 
     const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sanday'];
     const monthArray = [
         'January', 'February', 'March', 'April', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
     ];
 
-    timeInfoDate.innerHTML = `${dayArray[currentDay]}, ${monthArray[currentMonth]} ${currentYear}`;
+    mainDateValue.innerHTML = `${dayArray[currentDay]}, ${currentDayMonth} ${monthArray[currentMonth]}`;
 }
 getCurrentDate();
