3dd1b2649555072baaef4d90f7490f1f91492754
todolist-jsnode <victorprogramadorvasconcelos@gmail.com>
2022-08-21 11:34:24 +0100

    Update weather.js

--- src/js/api/weather.js
+++ src/js/api/weather.js
@@ -2,29 +2,32 @@ const keyAPI = `5f368db2d8ee713b9759b96de62cbf6e`;
 
 const searchInput = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-button');
+const cityInfoName = document.querySelector('.city-info-name');
+const countryInfoName = document.querySelector('.search-input-country');
 
 const createAPILogic = () => {
-    var cityLatitude, cityLongitude;
-
     const cityName = searchInput.value;
-    const geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${keyAPI}`;
 
-    const getGeocodingData = async () => {
-        const fetchGeocoding = await fetch(geocodingURL);
-        const jsonGeocodingData = await fetchGeocoding.json();
+    const getPositionData = async () => {
+        const geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${keyAPI}`;
+        
+        const geocodingFetch = await fetch(geocodingURL);
+        const geocodingData = await geocodingFetch.json();
 
-        cityLatitude = jsonGeocodingData[0].lat;
-        cityLongitude = jsonGeocodingData[0].lon;
+        const cityLatitude = geocodingData[0].lat;
+        const cityLongitude = geocodingData[0].lon;
+
+        cityInfoName.innerHTML = `${geocodingData[0].name}`;
 
         const getWeatherData = async () => {
             const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLatitude}&lon=${cityLongitude}&appid=${keyAPI}`;
-
+    
             const fetchWeather = await fetch(weatherURL);
             const jsonWeatherData = await fetchWeather.json();
-
+    
             console.log(jsonWeatherData);
         }
         getWeatherData();
     }
-    getGeocodingData();
+    getPositionData();
 }
